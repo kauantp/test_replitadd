3951cfb097908b507fc419e5cef071120b52d071
Kauan Torrezan Palazzi <kauanbr@yahoo.com>
2024-03-20 18:39:43 +0000

    changed code

--- main.c
+++ main.c
@@ -1,14 +1,2 @@
 #include <stdio.h>
-int main() {
-  int number;
-
-  printf("Enter an integer: ");
-
-  // reads and stores input
-  scanf("%d", &number);
-
-  // displays output
-  printf("You entered: %d", number);
-
-  return 0;
-}
+int main() { printf("Enter an integer: "); }
